1637b4d78c2105de6d642e2a1ecd3f3affd65c6a
Natasha Tolia <tolin478@newschool.edu>
2017-09-07 17:08:22 -0400

    First Week Homework

---
+++ Lab HW2 - Landscape/Assets/SphereMoving.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMoving : MonoBehaviour {
+
+		public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+		float moveSpeed = 4f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float move = Input.GetAxisRaw ("Vertical") * 2;
+		transform.Translate (move, 0, 0);
+
+		
+	}
+}
